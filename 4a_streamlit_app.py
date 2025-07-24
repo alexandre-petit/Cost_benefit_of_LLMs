8bf2c8b4dfad65132df5f2123e76e76e4eb09701
Alexandre <alexandrepetit8855@naver.com>
2025-07-24 21:36:36 +0200

    adding requirement and updating streamlit app

--- 4a_streamlit_app.py
+++ 4a_streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud, STOPWORDS
 
 # Sample data
 data = pd.read_csv("output_df.csv")
@@ -15,6 +18,42 @@ fig = px.bar(category_counts, x='category', y='count', title='Count by Category'
 st.plotly_chart(fig)
 
 #ajouter analyse descriptive et wordcould
+stopwords = set(STOPWORDS)
+
+def filter_words(word_list: list) -> list:
+    output_list = []
+    for word in word_list:
+        if len(word) > 2 and word not in stopwords:
+            output_list.append(word)
+    
+    return output_list
+
+data["words"] = data["name_description"].str.split(" ")
+
+data["words"] = [filter_words(l) for l in data["words"]]
+
+exploded_words = data.explode("words")
+
+text = " ".join(data['name_description'])
+
+word_counts = exploded_words["words"].value_counts().reset_index()
+word_counts.columns = ["Word", "Frequency"]
+
+st.subheader(" ".join(word_counts.columns))
+
+st.subheader("Most common words")
+fig_3 = px.bar(word_counts.head(10), y="Word", x="Frequency")
+st.plotly_chart(fig_3)
+
+wordcloud = WordCloud(width=800, height=400,
+                      background_color='white',
+                      stopwords=stopwords).generate(text)
+
+st.subheader("Wordcloud")
+fig_2, ax = plt.subplots(figsize=(10, 5))
+ax.imshow(wordcloud, interpolation='bilinear')
+ax.axis('off')
+st.pyplot(fig_2)
 
 # 2. First selector - Choose a category
 st.subheader("Select item to see model prediction")
